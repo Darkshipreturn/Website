b51e723b450c2e3ebb634a0969392f707a53e242
Darkshipreturn <74745096+Darkshipreturn@users.noreply.github.com>
2022-12-07 18:09:00 +1100

    Create title.js

---
+++ All Scripts/title.js
@@ -0,0 +1,17 @@
+document.write('\
+\
+<h2 class="main-title">\
+<img src="/media/images/default-black.png" alt="main logo" width="32" height="32" usemap="#workmap"> <a class="main-title-text" href="/">Darkshipreturn</a> \
+<span class="title-links">\
+<a class="t-links" href="/downloads">Download Center</a>\
+<a class="t-links" href="/fun">Funny</a>\
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.youtube.com/channel/UC1VSDiiRQZRTbxNvWhIrJfw"><img class="t-logos" src="/media/images/yt-logo.svg" alt="youtube-logo">Youtube</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.twitter.com/Darkshipreturn"><img class="t-logos" src="/media/images/twitter-logo.svg" alt="twitter-logo">Twitter</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.reddit.com/user/C468"><img class="t-logos" src="/media/images/reddit-logo.svg" alt="reddit-logo">Reddit</a>    \
+<a class="t-links" href="/bot"><img class="t-logos" src="/media/images/discord-logo.svg" alt="discord-logo">Discord Bot</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://twitch.tv/Darkshipreturn"><img class="t-logos" src="/media/images/twitch-logo.svg" alt="twitch-logo">Twitch</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://github.com/Darkshipreturn"><img class="t-logos" src="/media/images/github-logo.svg" alt="github-logo">Github</a>   \
+ </span></h2>\
+<map name="workmap">\
+  <area shape="default" coords="0,0,0,0" alt="Darkshipreturn" href="/">\
+</map>');
