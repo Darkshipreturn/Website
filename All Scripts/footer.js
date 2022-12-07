bcdba7cb64981c8a3db2bc8a5667bd967061efef
Darkshipreturn <74745096+Darkshipreturn@users.noreply.github.com>
2022-12-07 17:49:34 +1100

    Create footer.js

---
+++ All Scripts/footer.js
@@ -0,0 +1,5 @@
+document.write('\
+\
+<footer style="text-align: center;">\
+<p>Copyright © Darkshipreturn 2019-2022 <a target="_blank" rel="noreferrer noopener" href="https://github.com/Darkshipreturn/darkshipreturn.github.io">Source Code</a></p>\
+</footer>')
